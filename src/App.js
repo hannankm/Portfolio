0d0a19dc388275c6791f7b4218304c440cc3985f
Hanan kedir <hanankmoh@gmail.com>
2024-02-13 23:26:18 +0300

    Section components setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Experience from "./sections/Experience";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Contact />
+        <Footer />
       </header>
     </div>
   );
---
+++ src/sections/Skills.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <>
+      <h1>skills</h1>
+    </>
+  );
+};
+export default Skills;
