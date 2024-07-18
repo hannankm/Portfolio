6efd73c82d6a5aa088ac78ff22ddd1f37ff48ccf
Hanan kedir <hanankmoh@gmail.com>
2024-07-18 22:23:28 +0300

    Upload assets

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import hijabi from "./mypic.jpg";
+import database from "./database-storage1.png";
+import react from "./atom1.png";
+import mobile from "./coding1.png";
+import project1 from "./project1.webp";
+import project2 from "./project2.jpg";
+import project3 from "./project3.jpg";
+import moonImage from "./moon1.png";
+import code from "./code.png";
+import desk from "./working.png";
+import desk1 from "./working (1).png";
+
+export {
+  hijabi,
+  database,
+  react,
+  mobile,
+  project1,
+  project2,
+  project3,
+  moonImage,
+  code,
+  desk,
+  desk1,
+};
