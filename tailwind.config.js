3a8ca98423866c58c55ec67ccb3173f533a5e780
Hanan kedir <hanankmoh@gmail.com>
2024-07-18 22:24:58 +0300

    Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#070F2B",
+        beige: "#F8FAE5",
+        highlight: "#FE7A36",
+      },
+      keyframes: {
+        "spin-pause": {
+          "0%": { transform: "rotate(0deg)" },
+          "83.33%": { transform: "rotate(360deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        "spin-pause": "spin-pause 10s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
