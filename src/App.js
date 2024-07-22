e3b6f7f4075c60203c65d7f8a74ecdf86a9bd76c
Hanan kedir <hanankmoh@gmail.com>
2024-07-22 22:15:49 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import "./App.css";
-import Hero from "./sections/Hero";
-import NavBar from "./sections/NavBar";
-import About from "./sections/About";
-import Skills from "./sections/Skills";
-import Experience from "./sections/Experience";
-import Projects from "./sections/Projects";
-import Contact from "./sections/Contact";
-import Footer from "./sections/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <div className="text-beige bg-navy min-h-screen mx-auto">
-      <NavBar />
-      <Hero />
-      <About />
-      <Skills />
-      <Experience />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
